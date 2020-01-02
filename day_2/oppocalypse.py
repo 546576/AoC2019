f042c7f5e1c281284ab09a1f9d6c866642830805
546576 <wil.deering@gmail.com>
2020-01-02 17:29:31 +0000

    day 2 part 1: oppcode madness

---
+++ day_2/oppocalypse.py
@@ -0,0 +1,55 @@
+#reading the file and writing the contents to an array
+filepath = 'day_2/opcode.txt'
+array_position = [0, 1, 2, 3]
+array = []
+
+
+#setting up the array
+with open(filepath) as fp:
+        line = fp.readline()
+        final_tally = 0
+        n = 0
+
+        while line:
+            line_int = int(line)
+
+            array.insert(n, line_int)
+            line = fp.readline()
+            n += 1
+
+
+
+#adjusting array positions
+op_1 = array[array_position[0]]
+op_2 = array[array_position[1]]
+op_3 = array[array_position[2]]
+op_4 = array[array_position[3]]
+array[1] = 12
+array[2] = 2
+
+#running opcode
+if op_1 == 1:
+    add_op = array[op_2] + array[op_3]
+    array[op_4] = add_op
+    array_position[0] += 4
+    array_position[1] += 4
+    array_position[2] += 4
+    array_position[3] += 4
+
+elif op_1 == 2:
+    add_op = array[op_2] * array[op_3]
+    array[op_4] = add_op
+    array_position[0] += 4
+    array_position[1] += 4
+    array_position[2] += 4
+    array_position[3] += 4
+
+
+elif op_1 == 99:
+    print(array[0])
+
+
+
+
+
+fp.close()
